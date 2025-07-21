f7770fc0bed16bd0affa6f10d85fa41167736dda
57406 <57406@etu.he2b.be>
2025-07-21 14:22:02 +0200

    Walljumps feel good now with the coyote jump timer + fixed bug where wall grabbing animation would face wrong diretion for a frame

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var jump_height_variation_timer = $JumpHeightVariationTimer
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var wall_jump_height_variation_timer = $WallJumpHeightVariationTimer
+@onready var coyote_walljump_timer = $CoyoteWalljumpTimer
 
 const GRAVITY:int = 900
 const SPEED:int = 10000
@@ -13,9 +14,11 @@ enum State {IDLE, RUN, JUMP, FALL, WALL, WALLWALK}
 
 var current_state:State
 var is_facing_right:bool = true
+var last_wall_grabbed_on_right:bool
 var can_go_higher:bool
 var can_go_higher_wall:bool
 var was_on_floor:bool
+var was_on_wall:bool
 
 func _ready() -> void:
 	current_state = State.IDLE
@@ -29,11 +32,14 @@ func _physics_process(delta) -> void:
 	player_wall_jump(delta)
 	
 	was_on_floor = is_on_floor()
+	was_on_wall = is_on_wall()
 	move_and_slide()
 	if !is_on_floor() and was_on_floor and velocity.y >= 0:
 		coyote_jump_timer.start()
+	if !is_on_wall() and was_on_wall and velocity.y >= 0:
+		coyote_walljump_timer.start()
 	player_animation()
-	print(is_facing_right)
+	print(last_wall_grabbed_on_right)
 
 func player_falling(delta) -> void:
 	if !is_on_floor():
@@ -61,8 +67,9 @@ func player_run(delta) -> void:
 			velocity.x += direction * SPEED * delta / 15
 		else:
 			velocity.x = direction * SPEED * delta
-		if is_on_wall():
+		if is_on_wall() and is_on_floor():
 			current_state = State.WALLWALK
+			last_wall_grabbed_on_right = is_facing_right
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -81,22 +88,34 @@ func player_jump(delta) -> void:
 func player_slide_wall(delta) -> void:
 	if !is_on_wall() or is_on_floor():
 		return
+	var col = get_last_slide_collision()
+	if col == null:
+		return
+	var wall_normal: Vector2 = col.get_normal()
+	var wall_on_right = wall_normal.x < 0
+	var dir = Input.get_axis("move_left", "move_right")
+	if wall_on_right and dir <= 0:
+		return
+	if not wall_on_right and dir >= 0:
+		return
 	current_state = State.WALL
+	last_wall_grabbed_on_right = wall_on_right
 	velocity.y = 0
-	velocity.y += GRAVITY * delta / 2
+	velocity.y = min(velocity.y + GRAVITY * delta * 0.5, 200)
 	
 func player_wall_jump(delta) -> void:
-	if !is_on_wall() and !can_go_higher_wall:
+	if !is_on_wall() and !can_go_higher_wall and coyote_walljump_timer.is_stopped():
 		return
 	if Input.is_action_just_pressed("jump"):
-		if current_state == State.WALL or is_on_wall():
+		if current_state == State.WALL or is_on_wall() or !coyote_walljump_timer.is_stopped():
 			velocity.y = 0
 			velocity.x = 0
+			coyote_walljump_timer.stop()
 			wall_jump_height_variation_timer.start()
 			can_go_higher_wall = true
 			current_state = State.JUMP
 			velocity.y -= JUMP_HEIGHT * delta
-			if is_facing_right:
+			if last_wall_grabbed_on_right:
 				velocity.x -= SPEED * delta * 1.5
 			else:
 				velocity.x += SPEED * delta
@@ -120,5 +139,5 @@ func player_animation() -> void:
 func _on_jump_height_variation_timer_timeout() -> void:
 	can_go_higher = false
 
-func _on_wall_jump_height_variation_timer_timeout():
+func _on_wall_jump_height_variation_timer_timeout() -> void:
 	can_go_higher_wall = false
